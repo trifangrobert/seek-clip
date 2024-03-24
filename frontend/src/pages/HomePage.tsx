24e2a9d18023d15816931f09a9734b44e4c24304
Robert Trifan <trifangrobert@gmail.com>
2024-03-25 00:31:22 +0200

    add upload button on home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,18 +1,35 @@
 import VideoGrid from "../components/VideoGrid";
 import { useVideos } from "../hooks/useVideos";
 import { Loading } from "../components/Loading";
-import { Typography } from "@mui/material";
+import { Box, IconButton, Tooltip, Typography } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   const { videos, loading, error } = useVideos();
-
-  if (error) {
-    return <h1>{error}</h1>;
-  }
+  const navigate = useNavigate();
 
   return (
     <>
-      <Typography component="h1" variant="h4" sx={{m: 3}}>Home Page</Typography>
+      <Box sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        m: 4,
+      }}>
+        <Typography component="h1" variant="h4" sx={{ m: 1 }}>
+          Home Page
+        </Typography>
+        <Tooltip title="Upload Video" placement="right">
+          <IconButton onClick={() => navigate("/upload-video")} sx={{padding: "10px"}}>
+            <AddCircleOutlineIcon
+              sx={{ fontSize: "2.5rem", color: "#222831" }}
+            />
+          </IconButton>
+        </Tooltip>
+      </Box>
+
+      {error && <h1>{error}</h1>}
       {loading && <Loading />}
       {!loading && <VideoGrid videos={videos} />}
     </>
