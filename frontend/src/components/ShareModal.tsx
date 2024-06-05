8bc5d463d5223da3e215a342d8b504d146f52430
Robert Trifan <trifangrobert@gmail.com>
2024-06-05 18:36:52 +0300

    adjust share modal

--- frontend/src/components/ShareModal.tsx
+++ frontend/src/components/ShareModal.tsx
@@ -54,41 +54,47 @@ const ShareModal: React.FC<ShareModalProps> = ({ open, onClose, videoUrl }) => {
           left: "50%",
           transform: "translate(-50%, -50%)",
           width: 400,
+          height: 400,
           bgcolor: "background.paper",
           boxShadow: 24,
-          p: 4,
+          display: "flex",
+          flexDirection: "column",
         }}
       >
-        <Typography variant="h6" component="h2">
-          Share Video
-        </Typography>
-        <List>
-          {followers.map((follower) => (
-            <ListItem
-              onClick={() => handleShare(follower)}
-              key={follower._id}
-              sx={{
-                cursor: "pointer",
-                transition: "background-color 0.3s ease",
-                "&:hover": {
-                  backgroundColor:
-                    theme.currentTheme === "dark"
-                      ? "rgba(255, 255, 255, 0.2)"
-                      : "rgba(0, 0, 0, 0.2)",
-                },
-              }}
-            >
-              <ListItemAvatar>
-                <Avatar
-                  src={`${process.env.REACT_APP_API_URL}/${follower.profilePicture}`}
-                  alt={follower.username}
-                />
-              </ListItemAvatar>
-              <ListItemText primary={follower.username} />
-            </ListItem>
-          ))}
-        </List>
-        <Box textAlign="center" mt={2}>
+        <Box sx={{ p: 4, overflowY: "auto", flexGrow: 1 }}>
+          <Typography variant="h6" component="h2">
+            Share Video
+          </Typography>
+          <List>
+            {followers.map((follower) => (
+              <ListItem
+                onClick={() => handleShare(follower)}
+                key={follower._id}
+                sx={{
+                  cursor: "pointer",
+                  transition: "background-color 0.3s ease",
+                  "&:hover": {
+                    backgroundColor:
+                      theme.currentTheme === "dark"
+                        ? "rgba(255, 255, 255, 0.2)"
+                        : "rgba(0, 0, 0, 0.2)",
+                  },
+                }}
+              >
+                <ListItemAvatar>
+                  <Avatar
+                    src={`${process.env.REACT_APP_API_URL}/${follower.profilePicture}`}
+                    alt={follower.username}
+                  />
+                </ListItemAvatar>
+                <ListItemText primary={follower.username} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box textAlign="center" mt={2} sx={{ p: 2 }}>
+          {" "}
+          {/* Footer Box */}
           <Button variant="outlined" onClick={onClose}>
             Cancel
           </Button>
